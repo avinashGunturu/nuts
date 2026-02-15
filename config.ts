183b485708b1c10a50e4c02101b72be8cf2b18b7
avinash Gunturu <avinashgunturu123@gmail.com>
2026-02-15 17:17:29 +0530

    updated the backend url.

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-    BASE_URL: 'http://localhost:5001/api',
-    // BASE_URL: 'https://nutsb.onrender.com/api',
+    // BASE_URL: 'http://localhost:5001/api',
+    BASE_URL: 'https://nutsb.onrender.com/api',
     ENDPOINTS: {
         CONTACT: '/contact',
         CONTACT_FILTER: '/contact/filter',
